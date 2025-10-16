7cdfc5dcc34ec0bfefab467e2a40b04d5467a46f
RalphDev <108262757+Rannh02@users.noreply.github.com>
2025-10-16 14:02:22 +0800

    UPDATE css file name

--- resources/views/LoginSystem/CashierLogin.blade.php
+++ resources/views/LoginSystem/CashierLogin.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cashier Login</title>
-  <link rel="stylesheet" href="{{ asset('css/Cashier.css') }}">
+  <link rel="stylesheet" href="{{ asset('LoginSystemcss/Cashier.css') }}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
